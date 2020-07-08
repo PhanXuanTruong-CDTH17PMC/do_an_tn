49a02cc5fb5de02b11787faee1b96f0a7a0c9795
LePhuTan-CDTH17PMC <tangiao1999@gmail.com>
2020-07-08 12:34:40 +0700

    part 3

---
+++ resources/views/cu-dan/quan-he/them-moi-quan-he.blade.php
@@ -0,0 +1,34 @@
+@extends('layout')
+@section('title')
+    Thêm mới quan hệ
+@endsection
+@section('main-content')
+    <div class="row">
+        <div class="col-12">
+            <div class="page-title-box">
+                
+                    <h4 class="page-title">Thêm mới quan hệ</h4>
+                
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card-box">
+                <form
+                    
+                    <div class="form-group">
+                        
+                        <label>Tên quan hệ<span class="text-danger">*</span></label>
+                        <input type="text"  placeholder="Nhập tên bộ phận" class="form-control">
+                        
+                    </div>
+                    <div class="form-group text-left mb-0">
+                        <button type="submit" class="btn btn-success waves-effect waves-light">Lưu</button>
+                        <a href="#" class="btn btn-purple waves-effect waves-light">Hủy</a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
